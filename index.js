98f280ee54218f7c09833fde2356e412417d843c
Hidden <zzdhidden@gmail.com>
2011-03-18 22:57:01 +0800

    Parse str

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var qqwry = require("./build/default/qqwry.node").qqwry;
 
-var re = /(安徽|福建|甘肃|广东|广西|贵州|海南|河北|河南|黑龙江|湖北|湖南|吉林|江苏|江西|辽宁|内蒙古|宁夏|青海|山东|山西|陕西|四川|西藏|新疆|云南|浙江)?省?([^\s]+)/;
+var re = /(安徽|福建|甘肃|广东|广西|贵州|海南|河北|河南|黑龙江|湖北|湖南|吉林|江苏|江西|辽宁|内蒙古|宁夏|青海|山东|山西|陕西|四川|西藏|新疆|云南|浙江)?省?([^市\s]+市?)([^\s]*)[\s|]*([^\s]+)/;
 /* Query province and city */
 qqwry.prototype.q = function(ip){
 	var str = this.query(ip);
@@ -8,7 +8,9 @@ qqwry.prototype.q = function(ip){
 	if( str ) {
 		return {
 			province: str[1],
-			city: str[2]
+			city: str[2],
+			district: str[3],
+			vender: str[4]
 		};
 	}
 	return null;
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var qqwry = require("./index");
 var assert = require("assert");
 
-var qqwry = qqwry.open(__dirname + "/../voq/geodata/qqwry.dat");
+var qqwry = qqwry.open(__dirname + "/../zhejie/zhejie.com/geodata/qqwry.dat");
 
 var data = [
 	["115.193.152.250", "浙江", "杭州市"],
@@ -9,6 +9,7 @@ var data = [
 	["117.25.128.84", "福建", "厦门市"],
 	["222.73.68.35", "上海", "上海市"],
 	["203.198.23.69", "香港", "香港"],
+	["221.223.102.183", "北京市", "北京市"],
 ];
 
 data.forEach(function(area) {
