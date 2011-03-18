e1c8a2b7cfcb33d9389a117f5d309d490667efab
Hidden <zzdhidden@gmail.com>
2011-03-19 00:46:46 +0800

    Only province in data.

--- test.js
+++ test.js
@@ -10,10 +10,12 @@ var data = [
 	["222.73.68.35", "上海", "上海市"],
 	["203.198.23.69", "香港", "香港"],
 	["221.223.102.183", "北京市", "北京市"],
+	["211.66.210.249", "广东", ""],
 ];
 
 data.forEach(function(area) {
 	var q = qqwry.q(area[0]);
+	console.log(q);
 	q && assert.equal(q.city, area[2]);
 });
 
